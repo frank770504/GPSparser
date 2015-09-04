2de300700a9b2b291e5e2b57bbb36f32fb6d5685
frank.yw.chen <frank770504@gmail.com>
2015-09-04 15:07:08 +0800

    modify the kalman filter file for EKF feature
    I add two callback functions in the Kalman filter class
    
    EKF_mu_callback is to predict mu
    
    EKF_z_callback is to present observeratino

--- Kalman.py
+++ Kalman.py
@@ -40,23 +40,30 @@ class Kalman_filter:
 			self.R_cov = R_cov
 		if Q_cov != 'default':
 			self.Q_cov = Q_cov
-	def dump_matrices(dump):
+	def dump_matrices(self,dump):
 		self.dump = dump
-	def do_filter(self, mu, Sig, z, u):
+	def do_filter(self, mu, Sig, z, u, EKF_mu_callback='default', EKF_z_callback='default'):
 		from numpy.linalg import inv
 		mu = np.atleast_2d(mu).T
 		z = np.atleast_2d(z).T
 		u = np.atleast_2d(u).T
-		r, c = np.shape(self.A)
-		R = gaussian_noise_diag(0, self.R_cov, r)
-		r, c = np.shape(self.C)
-		Q = gaussian_noise_diag(0, self.Q_cov, r)
+		rA, cA = np.shape(self.A)
+		R = gaussian_noise_diag(0, self.R_cov, rA)
+		rC, cC = np.shape(self.C)
+		Q = gaussian_noise_diag(0, self.Q_cov, rC)
 		err = gaussian_noise_vec(0, self.err_cov, mu.shape)
-		mu_pre = self.A*mu + self.B*u + err.get_mat()
+		if EKF_mu_callback=='default':
+			mu_pre = self.A*mu + self.B*u + err.get_mat()
+		else:
+			mu_pre = EKF_mu_callback(mu) + err.get_mat()
 		Sig_pre = self.A*Sig*self.A.T + np.fabs(R.get_mat())
 		K = Sig_pre*self.C.T*inv(self.C*Sig_pre*self.C.T + np.fabs(Q.get_mat()))
-		mu_post = mu_pre + K*(z - self.C*mu_pre)
-		Sig_post = (np.eye(4) - K*self.C)*Sig_pre
+		if EKF_z_callback=='default':
+			pre_z = self.C*mu_pre
+		else:
+			pre_z = EKF_z_callback(mu_pre)
+		mu_post = mu_pre + K*(z - pre_z)
+		Sig_post = (np.eye(cC) - K*self.C)*Sig_pre
 		if self.dump == 'on':
 			print '{} mu'.format(mu.T)
 			print '{} Sig'.format(np.diag(Sig))
@@ -67,8 +74,8 @@ class Kalman_filter:
 		#	print '{} C*Sig_pre*C.T + Q'.format(inv(C*Sig_pre*C.T + Q))
 		#	print '{} Sig_pre*C'.format(Sig_pre*C.T)
 			print '{} K'.format(K)
-			print '{} R'.format(np.diag(R))
-			print '{} Q'.format(np.diag(Q))
+		#	print '{} R'.format(np.diag(R))
+		#	print '{} Q'.format(np.diag(Q))
 		#	print '{} K*(z - C*mu_pre)'.format((K*(z - C*mu_pre)).T)
 			print '{} mu_post'.format(mu_post.T)
 			print ''
